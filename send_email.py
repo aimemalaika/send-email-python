4db9361427f94f1d847988a8652e9ec411a08b93
Aime Malaika <aimemalaika1995@gmail.com>
2021-01-13 15:31:23 +0200

    sending email with dynamic data

--- send_email.py
+++ send_email.py
@@ -1,12 +1,17 @@
 import smtplib
 from email.message import EmailMessage
+from typing import ByteString
+from string import Template
+from pathlib import Path
+
+html = Template(Path('index.html').read_text())
 
 email = EmailMessage()
 email['from'] = 'Aime Malaika'
 email['to'] = 'aimemalaika1995@gmail.com'
 email['subject'] = 'You wan 1000000 dollars!'
 
-email.set_content('I am a python student!')
+email.set_content(html.substitute({'name':'TinTin'}),'html')
 
 with smtplib.SMTP(host='smtp.gmail.com',port=587) as smtp:
     smtp.ehlo()
